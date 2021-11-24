a0687a486f5a7bd58d5a8303b2f5f577f8d5073d
riyaraa <rinosatyaputra.id@gmail.com>
2021-11-24 18:43:37 +0700

    set up redux persist & axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { Provider } from "react-redux";
+import { store, persistor } from "store/store";
+import { PersistGate } from "redux-persist/integration/react";
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<Component {...pageProps} />
+				</PersistGate>
+			</Provider>
+		</>
+	);
 }
 
 export default MyApp;
