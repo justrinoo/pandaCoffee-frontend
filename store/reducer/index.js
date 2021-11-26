a37fe77c3129d4a1c1d0d2cb5abc9c9bdd8ac018
Aziz Imastara <sazizimastara@gmail.com>
2021-11-26 18:31:44 +0700

    pull

--- store/reducer/index.js
+++ store/reducer/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-<<<<<<< HEAD
 import auth from "./auth";
 import product from "./product";
 import cart from "./cart";
+import voucher from "./voucher";
 
 // ini reducernya masih kosong / persistnya masih error, kalo mau dibuat dulu biar si persist nya jalan juga.
 
@@ -10,10 +10,5 @@ export default combineReducers({
   auth,
   product,
   cart,
-=======
-import voucher from "./voucher";
-
-export default combineReducers({
-	voucher,
->>>>>>> 85c1c2186479527d489fd662aebabed2d3dd2068
+  voucher,
 });
