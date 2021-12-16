46975cc8d00a9596cb70d025eab4cf6b40dfb155
AndreasCKuriawan <andreasckurniawan01@gmail.com>
2021-12-16 11:13:33 +0700

    update : private Route

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-	reactStrictMode: true,
-	env: {
-		BASE_URL_DEV: "http://localhost:3001/",
-	},
-	// async rewrites() {
-	//   return [
-	//     {
-	//       source: "/product",
-	//       destination: "/admin/product",
-	//     },
-	//     {
-	//       source: "/productDetails/:productId",
-	//       destination: "/customer/productDetails/:productId",
-	//     },
-	//   ];
-	// },
+  reactStrictMode: true,
+  env: {
+    BASE_URL_DEV: "http://localhost:3001/",
+    BASE_URL_PROD: "https://coffe-shop-be.herokuapp.com/",
+    APP_HOST: "DEV",
+  },
+
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/product",
+  //       destination: "/admin/product",
+  //     },
+  //     {
+  //       source: "/productDetails/:productId",
+  //       destination: "/customer/productDetails/:productId",
+  //     },
+  //   ];
+  // },
 };
--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -11,39 +11,39 @@ import Cookie from "js-cookie";
 import { getDataCookie } from "middleware/authorizationPage";
 
 export async function getServerSideProps(context) {
-	const dataCookie = await getDataCookie(context);
-	if (!dataCookie.isLogin) {
-		return {
-			redirect: {
-				destination: "/auth/login",
-				permanent: false,
-			},
-		};
-	}
+  const dataCookie = await getDataCookie(context);
+  if (!dataCookie.isLogin) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+  }
 
-	return {
-		props: {},
-	};
+  return {
+    props: {},
+  };
 }
 
 export default function dashboardAdmin() {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<Layout title="Dashboard Admin">
-			<Navbar />
-			<main className="dashboard-admin">
-				<div className="row">
-					<div className="col-md-3"></div>
-					<div className="col-md-6">
-						<div className="chart-admin">
-							<Chart />
-						</div>
-					</div>
-					<div className="col-md-3"></div>
-				</div>
-			</main>
-			<Footer></Footer>
-		</Layout>
-	);
+  return (
+    <Layout title="Dashboard Admin">
+      <Navbar />
+      <main className="dashboard-admin">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <div className="chart-admin">
+              <Chart />
+            </div>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </main>
+      <Footer></Footer>
+    </Layout>
+  );
 }
